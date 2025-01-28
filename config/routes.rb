5be5123c6f23d5ed18f5f7f539bfa5befce81c5e
matinapap <matinapap180@gmail.com>
2025-01-28 17:26:08 +0200

    Generate PagesController. Initialize Home page

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  resources :posts
+  
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Health check route
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Root route
+  root to: 'pages#index'
+
+  # Render dynamic PWA files if needed (uncomment if used)
+  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+end
