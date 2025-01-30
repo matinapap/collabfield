d7e26be20b257a87c9502dd35ad5b5a561fdfa76
matinapap <matinapap180@gmail.com>
2025-01-30 23:36:58 +0200

    Configure and create helpers - Change include_all_helpers config to false - Split the _collapsible_elements.html.erb file's content into partials and extract logic from the file into partials

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module Collabfield
     # Αρχικοποίηση των προεπιλεγμένων ρυθμίσεων για τη γεννήτρια της έκδοσης Rails.
     config.load_defaults 8.0
 
+    config.action_controller.include_all_helpers = false
+
     # Ρύθμιση του Asset Pipeline μέσα στο after_initialize για να διασφαλίσουμε ότι όλα έχουν φορτωθεί
     config.after_initialize do
       Rails.application.config.assets.configure do |env|
