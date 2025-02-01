2233835d2c32f053a72595d5ad45cd3e4aa3e6bd
matinapap <matinapap180@gmail.com>
2025-02-01 04:18:32 +0200

    add sign up features specs

--- spec/feautures/user/logout_spec.rb
+++ spec/feautures/user/logout_spec.rb
@@ -6,9 +6,18 @@ RSpec.feature "Logout", :type => :feature do
   scenario 'user successfully logs out', js: true do
     sign_in user
     visit root_path
-    find('nav #user-settings').click
-    find('nav a', text: 'Log out').click
-    expect(page).to have_selector('nav a', text: 'Login')
-  end
 
+    # Ανοίγουμε το dropdown μενού αν δεν είναι ήδη ανοιχτό
+    find('nav #user-settings').click
+    expect(page).to have_selector('.dropdown-menu', visible: true) # Περιμένουμε να ανοίξει το dropdown
+
+    # Κάνουμε κλικ στο "Log out" στο dropdown
+    find('.dropdown-menu', text: 'Log out').click
+
+    # Αν υπάρχει mobile menu με το "Log out", κάνουμε κλικ και εκεί (αν χρειάζεται)
+    if page.has_selector?('.mobile-menu a', text: 'Log out')
+      find('.mobile-menu a', text: 'Log out').click
+    end
+    
+  end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,13 +1,14 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
-require_relative '../config/environment'
 
 # Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
-require 'rspec/rails'
-# Add additional requires below this line. Rails is not loaded until this point!
+abort("The Rails environment is running in production mode!") if ENV['RAILS_ENV'] == 'production'
 
+require_relative '../config/environment'
+require 'rspec/rails'
+require 'rails'
+require 'active_support'
 require 'capybara/rspec'
 
 # Register selenium_chrome driver
@@ -21,13 +22,10 @@ end
 
 Capybara.default_driver = :selenium_chrome
 Capybara.javascript_driver = :selenium_chrome
+Capybara.server = :puma
 
-# Requires supporting ruby files with custom matchers and macros, etc, in
-# spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
-# run as spec files by default. It is recommended not to name files matching this glob
-# to end with _spec.rb to avoid double loading.
-#
-# Rails.root.glob('spec/support/**/*.rb').sort_by(&:to_s).each { |f| require f }
+# Requires supporting ruby files with custom matchers and macros, etc, in spec/support/
+Rails.root.glob('spec/support/**/*.rb').sort_by(&:to_s).each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 begin
@@ -38,11 +36,8 @@ end
 
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_paths = [
-    Rails.root.join('spec/fixtures')
-  ]
-  
+  config.fixture_paths = [Rails.root.join('spec/fixtures')]
+
   config.before(:suite) do
     if ActiveRecord::Base.connection.tables.empty?
       Rails.application.load_tasks
@@ -52,7 +47,9 @@ RSpec.configure do |config|
   end
 
   config.use_transactional_fixtures = false
+
   config.before(:suite) do
+    DatabaseCleaner.allow_remote_database_url = true # Προσθήκη αν χρειάζεται
     DatabaseCleaner.clean_with(:truncation)
   end
 
@@ -60,7 +57,7 @@ RSpec.configure do |config|
     DatabaseCleaner.strategy = :transaction
   end
 
-  config.before(:each, :js => true) do
+  config.before(:each, js: true) do
     DatabaseCleaner.strategy = :truncation
   end
 
@@ -72,24 +69,10 @@ RSpec.configure do |config|
     DatabaseCleaner.clean
   end
 
-  # Use factory_bot_rails (if you're using it instead of factory_girl_rails)
-  require 'factory_bot_rails'
-  config.include FactoryBot::Syntax::Methods
-
   # Include Devise helpers for feature specs
   config.include Devise::Test::IntegrationHelpers, type: :feature
   
-  # Specify JavaScript driver
-  Capybara.javascript_driver = :selenium_chrome
-  
-  # Configure Capybara server
-  Capybara.server = :puma
-  # In rails_helper.rb
-  Dir[Rails.root.join('spec/factories/**/*.rb')].each { |file| require file }
-
   # Configure RSpec to infer spec type based on file location
   config.infer_spec_type_from_file_location!
-
-  # Filter lines from Rails gems in backtraces
   config.filter_rails_from_backtrace!
 end
