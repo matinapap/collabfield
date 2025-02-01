b7382651395f64094c66eaff7a7b0c9be2043db3
matinapap <matinapap180@gmail.com>
2025-02-01 22:12:19 +0200

    add sign up features specs

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_30_130557) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_01_163900) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.string "branch"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "content"
+    t.bigint "user_id", null: false
+    t.bigint "category_id", null: false
+    t.index ["category_id"], name: "index_posts_on_category_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -35,4 +45,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_30_130557) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "posts", "categories"
+  add_foreign_key "posts", "users"
 end
