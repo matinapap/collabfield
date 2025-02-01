a0b98fa67b9737ba0588da5086b6073f535f0d5b
matinapap <matinapap180@gmail.com>
2025-02-02 00:36:02 +0200

    Add CSS to posts on the home page - add CSS to the posts.scss file - add CSS to the mobile.scss to fix too large text issues on smaller screens

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,5 +4,7 @@ import { Turbo } from "turbo-rails"
 import { Application } from "stimulus"
 import { definitionsFromContext } from "stimulus/webpack-helpers"
 import "bootstrap"
+import "posts/style"
+
 
 // Από εδώ και πέρα, μπορείς να προσθέσεις επιπλέον JavaScript για τη λειτουργία της εφαρμογής
