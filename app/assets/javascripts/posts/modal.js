e098fe84868ae7e3ed6fa7a56b1af4354a2f2dba
matinapap <matinapap180@gmail.com>
2025-02-02 01:07:30 +0200

    Add a popup window to show a full post's content - Add bootstrap's modal component to show full post's content - Render the modal inside the home page's template - Add js to fill the modal with post's content and show it - Add CSS to style the modal

---
+++ app/assets/javascripts/posts/modal.js
@@ -0,0 +1,19 @@
+$(document).on('turbolinks:load', function() {
+    // When a post is clicked, show its full content in a modal window
+    $("body").on("click", ".single-post-card, .single-post-list", function() {
+      var posted_by = $(this).find('.post-content .posted-by').html();
+      var post_heading = $(this).find('.post-content h3').html();
+      var post_content = $(this).find('.post-content p').html();
+      var interested = $(this).find('.post-content .interested').attr('href');
+      
+      // Fill modal with data
+      $('.modal-header .posted-by').text(posted_by);
+      $('.loaded-data h3').text(post_heading);
+      $('.loaded-data p').text(post_content);
+      $('.loaded-data .interested a').attr('href', interested);
+      
+      // Show the modal
+      $('.myModal').modal('show');
+    });
+  });
+  
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,12 @@ class PostsController < ApplicationController
   def index
     @posts = Post.includes(:category).all
   end
+
+  def show
+    @post = Post.find_by(id: params[:id])
+    
+    unless @post
+      redirect_to posts_path, alert: "Το post δεν βρέθηκε."
+    end
+  end
 end
