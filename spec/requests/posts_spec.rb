15ec60f68a109f6030ab6726169a9f72813566ec
matinapap <matinapap180@gmail.com>
2025-02-04 01:41:40 +0200

    Add a spec for the Post model's default_scope

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -4,4 +4,11 @@ RSpec.describe "Posts", type: :request do
   describe "GET /index" do
     pending "add some examples (or delete) #{__FILE__}"
   end
+
+  context 'Scopes' do
+    it 'default_scope orders by descending created_at' do
+      first_post = create(:post)
+      second_post = create(:post)
+      expect(Post.all).to eq [second_post, first_post]
+  end
 end
